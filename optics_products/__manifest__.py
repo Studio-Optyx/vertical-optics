993003b5d28af1bf3c1b833373ec83fc4e6f4429
Fernando Plaza <fernando.plaza@enzo.es>
2025-03-28 11:18:29 +0100

    Cambios para que salgan los campos en una pestaña

--- optics_products/__manifest__.py
+++ optics_products/__manifest__.py
@@ -3,13 +3,14 @@
 
 {
     "name": "Vertical Optics Product Template",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Optics",
     "license": "AGPL-3",
     "author": "ENZO",
     "website": "https://www.enzo.es",
     "depends": [
         "base",
+        "product",
     ],
     "data": [
         "views/product_view.xml",
